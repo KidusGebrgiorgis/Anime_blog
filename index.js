385568fcde235398fe204055c6f38e283bbd3cb0
kgebrg1 <kgebrg1@students.towson.edu>
2021-07-22 14:51:25 -0400

    Creating own functionalities as well as using socket.io

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var express = require('express')
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 3001;
 
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+
 var messages = [
-        {name:"Kidus", message: "Yerr"},
-        {name:"Tom", message:"Whats good my boy"}
+        {name:"Kidus", message: "Yerr", like: 0},
+        {name:"Tom", message:"Whats good my boy", like: 1}
 ]
 
 app.use(express.static(__dirname))
@@ -18,7 +21,12 @@ app.use(bodyParser.urlencoded({extended: false}))
  })
  app.post("/message", (req,res) =>{
         messages.push(req.body)
+        io.emit("message", req.body)
         res.sendStatus(200)
  })
- app.listen(PORT, ()=>
+
+ io.on('connection', (socket) =>{
+        console.log("user connected")
+ })
+ http.listen(PORT, ()=>
     console.log('listening on port', PORT));
