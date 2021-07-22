dcfb1b72714cb0109f0f1856df0020d85d279e4c
kgebrg1 <kgebrg1@students.towson.edu>
2021-07-21 20:53:38 -0400

    Understanding interaction between server and client side

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-import express from 'express'
+var express = require('express')
+var bodyParser = require('body-parser')
 
 const app = express();
 const PORT = 3001;
 
-app.get('/', (req, res) =>
-        res.send('The app is functioning on port ${PORT}'));
-        
-app.listen(PORT, ()=>
+var messages = [
+        {name:"Kidus", message: "Yerr"},
+        {name:"Tom", message:"Whats good my boy"}
+]
+
+app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+ app.get("/message",(req,res) =>{
+        res.send(messages)
+ })
+ app.post("/message", (req,res) =>{
+        messages.push(req.body)
+        res.sendStatus(200)
+ })
+ app.listen(PORT, ()=>
     console.log('listening on port', PORT));
- 
