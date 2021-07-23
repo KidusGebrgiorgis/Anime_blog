bc6d9840e4870f1bd5a6015b8bb6a1f43253a5dc
kgebrg1 <kgebrg1@students.towson.edu>
2021-07-22 20:40:17 -0400

    Adding Database

--- index.js
+++ index.js
@@ -1,32 +1,50 @@
 var express = require('express')
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose')
 
 const app = express();
 const PORT = 3001;
+const dbURL = "mongodb+srv://kidusg:Password12..@chatapp.lqkh8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
 
-var messages = [
-        {name:"Kidus", message: "Yerr", like: 0},
-        {name:"Tom", message:"Whats good my boy", like: 1}
-]
+var Message = mongoose.model("messages",{
+       name: String,
+       message: String,
+       likes: Number
+})
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
  app.get("/message",(req,res) =>{
-        res.send(messages)
+        Message.find({},(err,message)=>{
+              res.send(message)     
+        })
  })
  app.post("/message", (req,res) =>{
-        messages.push(req.body)
-        io.emit("message", req.body)
-        res.sendStatus(200)
+        var message = new Message(req.body)
+        
+        message.save((err) =>{
+              if(err) sendStatus(500)
+
+              io.emit("message", req.body)
+              res.sendStatus(200)
+       })
  })
 
  io.on('connection', (socket) =>{
         console.log("user connected")
  })
+ mongoose.connect(dbURL,{  
+              useNewUrlParser: true,
+              useFindAndModify: false,
+              useUnifiedTopology: true,
+              useCreateIndex: true },(err)=>{
+        console.log("mongodb connected", err)
+ })
+
  http.listen(PORT, ()=>
     console.log('listening on port', PORT));
